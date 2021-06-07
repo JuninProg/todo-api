1d9845b80379f1b33cbc73340940ee2fd58d6f88
JuninProg <jose.lima.jjunior@gmail.com>
2021-06-07 18:21:32 -0300

    feat(migrations): improve todo_db

--- src/migrations/todo_db.sql
+++ src/migrations/todo_db.sql
@@ -1,9 +1,15 @@
 SET NAMES utf8;
 SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
 
-CREATE DATABASE `todo_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE `todo_db`;
 USE `todo_db`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT COLLATE=utf8mb4_unicode_ci DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `todo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,12 +22,4 @@ CREATE TABLE `todo` (
   PRIMARY KEY (`id`),
   KEY `fk_todo_user` (`user_id`),
   CONSTRAINT `fk_todo_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT COLLATE=utf8mb4_unicode_ci DEFAULT CHARSET=utf8mb4;
